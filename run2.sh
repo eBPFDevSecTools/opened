dbe40d11320d66f61df15a5b61fcc8e260b242a3
Sayandeep Sen <sayandes@in.ibm.com>
2022-11-21 04:53:43 +0000

    minor mods

--- run2.sh
+++ run2.sh
@@ -1,1 +1,1 @@
-python3 function-extractor.py -o op/katran -f asset/func.out.cleaned -e extracted.c -c cscope.files -t txl -s examples/katran
+python3 function-extractor.py -o op/katran -e extracted.c -c cscope.files -t txl -s examples/katran
