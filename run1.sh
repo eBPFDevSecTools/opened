5cc4b1ed02a79866fa9b40bf169c72859c390dd4
Sayandeep Sen <sayandes@in.ibm.com>
2022-12-03 11:50:49 +0000

    minor mods

--- run1.sh
+++ run1.sh
@@ -1,2 +1,2 @@
 #python3 extraction_runner.py -f xdpdecap -o txl -s examples/katran
-python3 extraction_runner.py -f cil_from_overlay -o txl_cilium -s examples/cilium
+python3 extraction_runner.py -f handle_ipv4 -o txl_cilium -s examples/cilium
