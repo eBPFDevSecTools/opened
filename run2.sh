edabdd89907e31a544b0e957b8fb8c9f4b4d031b
Sayandeep Sen <sayandes@in.ibm.com>
2022-12-05 05:00:51 +0000

    minor mods to Makefile

--- run2.sh
+++ run2.sh
@@ -1,2 +1,3 @@
 #python3 function-extractor.py -o op/katran -e extracted.c -c asset/func.out.cleaned -t txl -s examples/katran
-python3 function-extractor.py -o op/katran -e extracted.c -c func.out -t txl -s examples/katran
+#python3 function-extractor.py -o op/katran -e extracted.c -c func.out -t txl -s examples/katran
+python3 function-extractor.py -o op/cilium -e extracted.c -c func.out -t txl_cilium -s examples/cilium
