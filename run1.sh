1d1479399666974d37eaa4515e97625d3a2f04d8
Sayandeep Sen <sayandes@in.ibm.com>
2022-12-15 15:55:56 +0530

    Update run1.sh
    Signed-off-by: Sayandeep Sen <sayandes@in.ibm.com>

--- run1.sh
+++ run1.sh
@@ -1,2 +1,2 @@
-python3 annotator.py -o op/txl_katran -s examples/katran -c commented_katran -t op/katran/katran.function_file_list.json -u op/katran/katran.struct_file_list.json 
-python3 annotator.py -o op/txl_cilium -s examples/cilium -c commented_cilium -t op/cilium/cilium.function_file_list.json -u op/cilium/cilium.struct_file_list.json --isCilium 
+python3 annotator.py -o op/katran/txl_katran -s examples/katran -c op/katran/commented_katran -t op/katran/katran.function_file_list.json -u op/katran/katran.struct_file_list.json 
+python3 annotator.py -o op/cilium/txl_cilium -s examples/cilium -c op/cilium/commented_cilium -t op/cilium/cilium.function_file_list.json -u op/cilium/cilium.struct_file_list.json --isCilium 
