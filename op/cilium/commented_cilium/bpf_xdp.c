bd6f591dcc3ff5b0d741a7f4e3883557485ac16b
Sayandeep Sen <sayandes@in.ibm.com>
2022-12-16 08:15:33 +0000

    pushing generated artefacts for katran and cilium

---
+++ op/cilium/commented_cilium/bpf_xdp.c
@@ -0,0 +1,507 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#include <bpf/ctx/xdp.h>
+#include <bpf/api.h>
+
+#include <node_config.h>
+#include <netdev_config.h>
+#include <filter_config.h>
+
+#define SKIP_POLICY_MAP 1
+
+/* Controls the inclusion of the CILIUM_CALL_HANDLE_ICMP6_NS section in the
+ * bpf_lxc object file.
+ */
+#define SKIP_ICMPV6_NS_HANDLING
+
+/* Controls the inclusion of the CILIUM_CALL_SEND_ICMP6_TIME_EXCEEDED section
+ * in the bpf_lxc object file. This is needed for all callers of
+ * ipv6_local_delivery, which calls into the IPv6 L3 handling.
+ */
+#define SKIP_ICMPV6_HOPLIMIT_HANDLING
+
+/* Controls the inclusion of the CILIUM_CALL_SEND_ICMP6_ECHO_REPLY section in
+ * the bpf_lxc object file.
+ */
+#define SKIP_ICMPV6_ECHO_HANDLING
+
+/* Controls the inclusion of the CILIUM_CALL_SRV6 section in the object file.
+ */
+#define SKIP_SRV6_HANDLING
+
+/* The XDP datapath does not take care of health probes from the local node,
+ * thus do not compile it in.
+ */
+#undef ENABLE_HEALTH_CHECK
+
+#include "lib/common.h"
+#include "lib/maps.h"
+#include "lib/eps.h"
+#include "lib/events.h"
+#include "lib/nodeport.h"
+
+#ifdef ENABLE_PREFILTER
+#ifndef HAVE_LPM_TRIE_MAP_TYPE
+# undef CIDR4_LPM_PREFILTER
+# undef CIDR6_LPM_PREFILTER
+#endif
+
+#ifdef CIDR4_FILTER
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, struct lpm_v4_key);
+	__type(value, struct lpm_val);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+	__uint(max_entries, CIDR4_HMAP_ELEMS);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+} CIDR4_HMAP_NAME __section_maps_btf;
+
+#ifdef CIDR4_LPM_PREFILTER
+struct {
+	__uint(type, BPF_MAP_TYPE_LPM_TRIE);
+	__type(key, struct lpm_v4_key);
+	__type(value, struct lpm_val);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+	__uint(max_entries, CIDR4_LMAP_ELEMS);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+} CIDR4_LMAP_NAME __section_maps_btf;
+
+#endif /* CIDR4_LPM_PREFILTER */
+#endif /* CIDR4_FILTER */
+
+#ifdef CIDR6_FILTER
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, struct lpm_v6_key);
+	__type(value, struct lpm_val);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+	__uint(max_entries, CIDR4_HMAP_ELEMS);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+} CIDR6_HMAP_NAME __section_maps_btf;
+
+#ifdef CIDR6_LPM_PREFILTER
+struct {
+	__uint(type, BPF_MAP_TYPE_LPM_TRIE);
+	__type(key, struct lpm_v6_key);
+	__type(value, struct lpm_val);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+	__uint(max_entries, CIDR4_LMAP_ELEMS);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+} CIDR6_LMAP_NAME __section_maps_btf;
+#endif /* CIDR6_LPM_PREFILTER */
+#endif /* CIDR6_FILTER */
+#endif /* ENABLE_PREFILTER */
+
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 96,
+ Endline: 117,
+ Funcname: bpf_xdp_exit,
+ Input: (struct  __ctx_buff *ctx, const int verdict),
+ Output: int,
+ Helpers: [],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline __maybe_unused int
+bpf_xdp_exit(struct __ctx_buff *ctx, const int verdict)
+{
+	if (verdict == CTX_ACT_OK) {
+		__u32 meta_xfer = ctx_load_meta(ctx, XFER_MARKER);
+
+		/* We transfer data from XFER_MARKER. This specifically
+		 * does not break packet trains in GRO.
+		 */
+		if (meta_xfer) {
+			if (!ctx_adjust_meta(ctx, -(int)sizeof(meta_xfer))) {
+				__u32 *data_meta = ctx_data_meta(ctx);
+				__u32 *data = ctx_data(ctx);
+
+				if (!ctx_no_room(data_meta + 1, data))
+					data_meta[0] = meta_xfer;
+			}
+		}
+	}
+
+	return verdict;
+}
+
+#ifdef ENABLE_IPV4
+#ifdef ENABLE_NODEPORT_ACCELERATION
+__section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV4_FROM_NETDEV)
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 122,
+ Endline: 140,
+ Funcname: tail_lb_ipv4,
+ Input: (struct  __ctx_buff *ctx),
+ Output: int,
+ Helpers: [tail_call,],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+int tail_lb_ipv4(struct __ctx_buff *ctx)
+{
+	int ret = CTX_ACT_OK;
+
+	if (!bpf_skip_nodeport(ctx)) {
+		ret = nodeport_lb4(ctx, 0);
+		if (ret == NAT_46X64_RECIRC) {
+			ep_tail_call(ctx, CILIUM_CALL_IPV6_FROM_NETDEV);
+			return send_drop_notify_error(ctx, 0, DROP_MISSED_TAIL_CALL,
+						      CTX_ACT_DROP,
+						      METRIC_INGRESS);
+		} else if (IS_ERR(ret)) {
+			return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
+						      METRIC_INGRESS);
+		}
+	}
+
+	return bpf_xdp_exit(ctx, ret);
+}
+
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 142,
+ Endline: 147,
+ Funcname: check_v4_lb,
+ Input: (struct  __ctx_buff *ctx),
+ Output: int,
+ Helpers: [tail_call,],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline int check_v4_lb(struct __ctx_buff *ctx)
+{
+	ep_tail_call(ctx, CILIUM_CALL_IPV4_FROM_NETDEV);
+	return send_drop_notify_error(ctx, 0, DROP_MISSED_TAIL_CALL, CTX_ACT_DROP,
+				      METRIC_INGRESS);
+}
+#else
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 149,
+ Endline: 152,
+ Funcname: check_v4_lb,
+ Input: (struct  __ctx_buff * ctx __maybe_unused),
+ Output: int,
+ Helpers: [],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline int check_v4_lb(struct __ctx_buff *ctx __maybe_unused)
+{
+	return CTX_ACT_OK;
+}
+#endif /* ENABLE_NODEPORT_ACCELERATION */
+
+#ifdef ENABLE_PREFILTER
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 156,
+ Endline: 179,
+ Funcname: check_v4,
+ Input: (struct  __ctx_buff *ctx),
+ Output: int,
+ Helpers: [map_lookup_elem,],
+ Read_maps: [ CIDR4_LMAP_NAME, CIDR4_HMAP_NAME,],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline int check_v4(struct __ctx_buff *ctx)
+{
+	void *data_end = ctx_data_end(ctx);
+	void *data = ctx_data(ctx);
+	struct iphdr *ipv4_hdr = data + sizeof(struct ethhdr);
+	struct lpm_v4_key pfx __maybe_unused;
+
+	if (ctx_no_room(ipv4_hdr + 1, data_end))
+		return CTX_ACT_DROP;
+
+#ifdef CIDR4_FILTER
+	memcpy(pfx.lpm.data, &ipv4_hdr->saddr, sizeof(pfx.addr));
+	pfx.lpm.prefixlen = 32;
+
+#ifdef CIDR4_LPM_PREFILTER
+	if (map_lookup_elem(&CIDR4_LMAP_NAME, &pfx))
+		return CTX_ACT_DROP;
+#endif /* CIDR4_LPM_PREFILTER */
+	return map_lookup_elem(&CIDR4_HMAP_NAME, &pfx) ?
+		CTX_ACT_DROP : check_v4_lb(ctx);
+#else
+	return check_v4_lb(ctx);
+#endif /* CIDR4_FILTER */
+}
+#else
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 181,
+ Endline: 184,
+ Funcname: check_v4,
+ Input: (struct  __ctx_buff *ctx),
+ Output: int,
+ Helpers: [],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline int check_v4(struct __ctx_buff *ctx)
+{
+	return check_v4_lb(ctx);
+}
+#endif /* ENABLE_PREFILTER */
+#endif /* ENABLE_IPV4 */
+
+#ifdef ENABLE_IPV6
+#ifdef ENABLE_NODEPORT_ACCELERATION
+__section_tail(CILIUM_MAP_CALLS, CILIUM_CALL_IPV6_FROM_NETDEV)
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 191,
+ Endline: 203,
+ Funcname: tail_lb_ipv6,
+ Input: (struct  __ctx_buff *ctx),
+ Output: int,
+ Helpers: [],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+int tail_lb_ipv6(struct __ctx_buff *ctx)
+{
+	int ret = CTX_ACT_OK;
+
+	if (!bpf_skip_nodeport(ctx)) {
+		ret = nodeport_lb6(ctx, 0);
+		if (IS_ERR(ret))
+			return send_drop_notify_error(ctx, 0, ret, CTX_ACT_DROP,
+						      METRIC_INGRESS);
+	}
+
+	return bpf_xdp_exit(ctx, ret);
+}
+
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 205,
+ Endline: 210,
+ Funcname: check_v6_lb,
+ Input: (struct  __ctx_buff *ctx),
+ Output: int,
+ Helpers: [tail_call,],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline int check_v6_lb(struct __ctx_buff *ctx)
+{
+	ep_tail_call(ctx, CILIUM_CALL_IPV6_FROM_NETDEV);
+	return send_drop_notify_error(ctx, 0, DROP_MISSED_TAIL_CALL, CTX_ACT_DROP,
+				      METRIC_INGRESS);
+}
+#else
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 212,
+ Endline: 215,
+ Funcname: check_v6_lb,
+ Input: (struct  __ctx_buff * ctx __maybe_unused),
+ Output: int,
+ Helpers: [],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline int check_v6_lb(struct __ctx_buff *ctx __maybe_unused)
+{
+	return CTX_ACT_OK;
+}
+#endif /* ENABLE_NODEPORT_ACCELERATION */
+
+#ifdef ENABLE_PREFILTER
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 219,
+ Endline: 242,
+ Funcname: check_v6,
+ Input: (struct  __ctx_buff *ctx),
+ Output: int,
+ Helpers: [map_lookup_elem,],
+ Read_maps: [ CIDR6_LMAP_NAME, CIDR6_HMAP_NAME,],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline int check_v6(struct __ctx_buff *ctx)
+{
+	void *data_end = ctx_data_end(ctx);
+	void *data = ctx_data(ctx);
+	struct ipv6hdr *ipv6_hdr = data + sizeof(struct ethhdr);
+	struct lpm_v6_key pfx __maybe_unused;
+
+	if (ctx_no_room(ipv6_hdr + 1, data_end))
+		return CTX_ACT_DROP;
+
+#ifdef CIDR6_FILTER
+	__bpf_memcpy_builtin(pfx.lpm.data, &ipv6_hdr->saddr, sizeof(pfx.addr));
+	pfx.lpm.prefixlen = 128;
+
+#ifdef CIDR6_LPM_PREFILTER
+	if (map_lookup_elem(&CIDR6_LMAP_NAME, &pfx))
+		return CTX_ACT_DROP;
+#endif /* CIDR6_LPM_PREFILTER */
+	return map_lookup_elem(&CIDR6_HMAP_NAME, &pfx) ?
+		CTX_ACT_DROP : check_v6_lb(ctx);
+#else
+	return check_v6_lb(ctx);
+#endif /* CIDR6_FILTER */
+}
+#else
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 244,
+ Endline: 247,
+ Funcname: check_v6,
+ Input: (struct  __ctx_buff *ctx),
+ Output: int,
+ Helpers: [],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline int check_v6(struct __ctx_buff *ctx)
+{
+	return check_v6_lb(ctx);
+}
+#endif /* ENABLE_PREFILTER */
+#endif /* ENABLE_IPV6 */
+
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 251,
+ Endline: 278,
+ Funcname: check_filters,
+ Input: (struct  __ctx_buff *ctx),
+ Output: int,
+ Helpers: [],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline int check_filters(struct __ctx_buff *ctx)
+{
+	int ret = CTX_ACT_OK;
+	__u16 proto;
+
+	if (!validate_ethertype(ctx, &proto))
+		return CTX_ACT_OK;
+
+	ctx_store_meta(ctx, XFER_MARKER, 0);
+	bpf_skip_nodeport_clear(ctx);
+
+	switch (proto) {
+#ifdef ENABLE_IPV4
+	case bpf_htons(ETH_P_IP):
+		ret = check_v4(ctx);
+		break;
+#endif /* ENABLE_IPV4 */
+#ifdef ENABLE_IPV6
+	case bpf_htons(ETH_P_IPV6):
+		ret = check_v6(ctx);
+		break;
+#endif /* ENABLE_IPV6 */
+	default:
+		break;
+	}
+
+	return bpf_xdp_exit(ctx, ret);
+}
+
+__section("from-netdev")
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/bpf_xdp.c,
+ Startline: 281,
+ Endline: 284,
+ Funcname: bpf_xdp_entry,
+ Input: (struct  __ctx_buff *ctx),
+ Output: int,
+ Helpers: [],
+ Read_maps: [],
+ Update_maps: [],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+int bpf_xdp_entry(struct __ctx_buff *ctx)
+{
+	return check_filters(ctx);
+}
+
+BPF_LICENSE("Dual BSD/GPL");
---
+++ op/cilium/commented_cilium/bytecount.h
@@ -0,0 +1,41 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* Copyright Authors of Cilium */
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, __u32);
+	__type(value, __u64);
+	__uint(max_entries, 1024);
+} bytecount_map __section_maps_btf;
+
+/* 
+ OPENED COMMENT BEGIN 
+ { 
+ File: /home/sayandes/opened_extraction/examples/cilium/custom/bytecount.h,
+ Startline: 11,
+ Endline: 24,
+ Funcname: custom_prog,
+ Input: (const struct  __ctx_buff *ctx, __u32 identity),
+ Output: void,
+ Helpers: [map_lookup_elem,map_update_elem,],
+ Read_maps: [  bytecount_map,],
+ Update_maps: [ bytecount_map,],
+ Func Description: TO BE ADDED, 
+ Commentor: TO BE ADDED (<name>,<email>) 
+ } 
+ OPENED COMMENT END 
+ */ 
+static __always_inline
+void custom_prog(const struct __ctx_buff *ctx, __u32 identity)
+{
+	__u64 len, *bytecount;
+
+	len = ctx_full_len(ctx);
+
+	bytecount = map_lookup_elem(&bytecount_map, &identity);
+	if (bytecount)
+		__sync_fetch_and_add(bytecount, len);
+	else
+		/* No entry for endpoint in hashmap, attempt to create one */
+		map_update_elem(&bytecount_map, &identity, &len, BPF_ANY);
+}
